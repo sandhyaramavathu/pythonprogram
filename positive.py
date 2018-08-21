e7e10382c689db273bb892bdc7838a75778896cb
sandhyaramavathu <42571611+sandhyaramavathu@users.noreply.github.com>
2018-08-21 16:54:40 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,8 @@
+# your code goes here
+num = int(input("enter the number"))
+if num < 0:
+    print("the number is negative")
+elif num == 0:
+    print("the number is zero")
+else:
+    print("the given number is positive")
